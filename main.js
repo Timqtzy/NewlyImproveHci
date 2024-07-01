8d2cdc3816dfdaa31377bcaea91659b0dd817eca
Timqtzy <100458231+Timqtzy@users.noreply.github.com>
2024-07-01 19:39:26 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+ScrollReveal({ 
+    reset: true,
+    distance: '60px',
+    duration: 1000,
+    delay: 400
+});
+
+/*Home*/
+ScrollReveal().reveal('.container1', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.container2', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.container3', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.container4', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.container5', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.container6', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.container7', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.container8', { delay: 500, origin: 'left' });
+
+/*News*/
+ScrollReveal().reveal('.content-news', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.content2-news', { delay: 500, origin: 'left' });
+
+
+/*Gallery*/
+ScrollReveal().reveal( '.Container img', { delay: 500, origin: 'left' });
+
+/*About*/
+ScrollReveal().reveal('.aboutpage1', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.aboutpage2', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.aboutpage3', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.aboutpage4', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.aboutpage5', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.teacher1', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.teacher2', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.teacher3', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.teacher4', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.StudentCouncil img', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.StudentCouncil div', { delay: 500, origin: 'left' });
+
+/*Contact*/
+ScrollReveal().reveal('.ContactPage', { delay: 500, origin: 'left' });
+
+
+/*Almost finish still and here I am crying over our about page huhu*/
